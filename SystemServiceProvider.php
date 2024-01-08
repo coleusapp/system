cae184c681e888f056750925d3d0008ebefa6b9e
Amirmasoud <amirmasood33@gmail.com>
2024-01-08 00:15:15 -0700

    wip

--- SystemServiceProvider.php
+++ SystemServiceProvider.php
@@ -11,9 +11,9 @@ class SystemServiceProvider extends ServiceProvider
         $this->loadRoutesFrom(__DIR__.'/routes/web.php');
         $this->loadViewsFrom(__DIR__.'/resources/views', 'system');
         $this->mergeConfigFrom(__DIR__.'/config/system.php', 'system');
-        $this->publishes([
-            __DIR__.'/resources/dist' => public_path('dist/coleus/system')
-        ], 'dist');
+        // $this->publishes([
+        //     __DIR__.'/resources/dist' => public_path('dist/coleus/system')
+        // ], 'dist');
 
         $this->app->bind('system', function ($app) {
             return new System();
